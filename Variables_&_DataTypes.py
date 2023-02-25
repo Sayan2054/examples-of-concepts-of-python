5999fae259d423da6148ec1da7f6ee144aad3520
Sayan2054 <125724299+Sayan2054@users.noreply.github.com>
2023-02-25 22:58:55 +0530

    variables and data types
    These are some of the examples of variables and data types

---
+++ Variables_&_DataTypes.py
@@ -0,0 +1,32 @@
+#Some examples of variables and data types are:-
+
+#Integers:-
+x = 5
+y = -10
+z = 0
+
+#Floats:-
+a = 3.14
+b = -2.5
+c = 0.0
+
+#Strings:-
+name = "John"
+address = '123 Main Street'
+message = "It's a beautiful day today."
+
+#Booleans:-
+is_sunny = True
+is_raining = False
+
+#Lists:-
+fruits = ["apple", "banana", "cherry"]
+numbers = [1, 2, 3, 4, 5]
+mixed = [1, "apple", True, 3.14]
+
+#Tuples:-
+person = ("John", 25, "New York")
+coordinates = (40.7128, -74.0060)
+
+#Dictionaries:-
+person = {"name": "John", "age": 25, "city": "New York"}
