d74c65ae263e03f518a5b11777d789157719493f
Sayan2054 <125724299+Sayan2054@users.noreply.github.com>
2023-02-25 23:41:45 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,28 @@
+#Examples of loops
+
+#for loop:-
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+'''output:-
+apple
+banana
+cherry'''
+
+#while loop:-
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+'''output:-
+1
+2
+3
+4
+5
+'''
+
+
+
