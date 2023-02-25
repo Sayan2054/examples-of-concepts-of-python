91b9c89eccf3932760e459dbd61dee87f0db7800
Sayan2054 <125724299+Sayan2054@users.noreply.github.com>
2023-02-25 23:26:47 +0530

    Conditional statements

---
+++ Conditional_statements.py
@@ -0,0 +1,25 @@
+#Examples of conditional statements
+
+#if statement:-
+num = 10
+if num > 0:
+    print("Positive number")
+#output - Positive number
+
+#if-else statement:-
+num = -5
+if num > 0:
+    print("Positive number")
+else:
+    print("Negative number")
+#output - Negative number
+
+#if-elif-else statement:-
+num = 0
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
+#output - Zero
