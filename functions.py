7b7f2a11b2ff8bb8132cdf6ea1f2088f3c381e5d
Sayan2054 <125724299+Sayan2054@users.noreply.github.com>
2023-02-26 09:02:55 +0530

    functions.py
    In this example, a function called "greet" is defined that takes a parameter called "name". When the function is called with the argument "John", it will print "Hello, John. How are you doing today?" to the console. The function also includes a docstring that describes what the function does.

---
+++ functions.py
@@ -0,0 +1,14 @@
+#Examples of functions are:-
+
+#Functions:-
+def greet(name):
+    """
+    This function greets the person passed in as a parameter.
+    """
+    print("Hello, " + name + ". How are you doing today?")
+
+greet("John")
+
+'''Outout:-
+Hello, John. How are you doing today?
+'''
